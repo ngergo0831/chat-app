dc5308f839b47012c71e352eb82090c4f17aa65a
ngergo0831 <ngergo0831@gmail.com>
2021-03-27 11:28:38 +0100

    Created database connection

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,21 @@ import * as express from "express";
 import router from "./router";
 import * as morgan from "morgan";
 import * as bodyParser from "body-parser";
+import { createConnection } from "typeorm";
 
 export const app = express();
 
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV !== "PRODUCTION")
-{
-    app.use(morgan("dev"));
+createConnection({
+  type: "sqlite",
+  database: "db.sqlite",
+  entities: ["dist/**/*.entity.js"],
+  synchronize: true,
+});
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  app.use(morgan("dev"));
 }
 
 app.use(router);
